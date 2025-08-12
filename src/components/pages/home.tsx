cb23d7d307b17bf17aa9b2aea9977f1ebc0e5523
LeynardPenaranda <leynardlove@gmail.com>
2025-08-12 09:24:57 +0800

    added profile in home

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -2,6 +2,7 @@ import type { RootState } from "store";
 import AnimationWrapper from "../animation/animation-wrapper-page";
 import HomeCarousel from "../home-carousel";
 import { useSelector } from "react-redux";
+import Username from "@/features/user/username";
 
 const Home = () => {
   const username = useSelector((state: RootState) => state.user.username);
@@ -10,9 +11,17 @@ const Home = () => {
       <div className="max-w-7xl mx-auto">
         <div className="mb-10 h-35 flex items-center">
           {!username ? (
-            <h1 className="font-bold">Welcome, but Please Enter your name</h1>
+            <h1 className="font-medium text-2xl">
+              Welcome, but Please Enter your name
+            </h1>
           ) : (
-            <p>Welcome back, {username}!</p>
+            <div>
+              <Username
+                message="Hello, "
+                classNameParagraph="font-medium text-2xl"
+                classNameAvatar="h-15 w-15"
+              />
+            </div>
           )}
         </div>
         <HomeCarousel />
--- src/features/user/username.tsx
+++ src/features/user/username.tsx
@@ -4,20 +4,31 @@ import UserDialog from "./user-dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { User } from "lucide-react";
 
-const Username = () => {
+const Username = ({
+  classNameParagraph,
+  classNameAvatar,
+  message,
+}: {
+  classNameParagraph?: string;
+  classNameAvatar?: string;
+  message?: string;
+}) => {
   const username = useSelector((state: RootState) => state.user.username);
 
   return (
     <>
       {username ? (
         <div className="flex items-center gap-2">
-          <Avatar>
+          <Avatar className={`${classNameAvatar}`}>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>
               <User />
             </AvatarFallback>
           </Avatar>
-          <p>{username}</p>
+          <p className={`${classNameParagraph}`}>
+            {message}
+            {username}
+          </p>
         </div>
       ) : (
         <UserDialog />
